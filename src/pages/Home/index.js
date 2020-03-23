5db077f0c3feef7474e1d77b80bb437d44c5e656
Luka Tassano <lukatassano070@gmail.com>
2020-03-23 09:03:07 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_detalhe1.jpg?ts=1562855980?ims=280x280" alt="Tenis"/>
+        <strong>Tenis </strong>
+      </li>
+    </ProductList>
+  );
+}
