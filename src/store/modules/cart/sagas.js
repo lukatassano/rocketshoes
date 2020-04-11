ee9f55388733f0b637fc758e63355077f5218eb0
Luka Tassano <lukatassano070@gmail.com>
2020-04-11 02:53:44 -0300

    error message if the quantity exceeds stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,17 +3,32 @@ import { call, select, put, all, takeLatest } from "redux-saga/effects";
 import api from '../../../services/api';
 import {formatPrice} from '../../../util/format';
 
-import {addToCartSuccess, updateAmount} from './actions';
+import {addToCartSuccess, updateAmountSuccess} from './actions';
+import { toast } from "react-toastify";
 
 function* addToCart({id}) {
   const productExists = yield select(
     state => state.cart.find(p => p.id === id),
   )
 
+  const stock = yield call(api.get, `stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque')
+    return;
+    
+  }
+
   if (productExists) {
     const amount = productExists.amount + 1;
 
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`)
 
@@ -29,6 +44,20 @@ function* addToCart({id}) {
   
 }
 
+function* updateAmount({id, amount}) {
+  if ( amount <= 0 ) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if ( amount > stockAmount ) {
+    return toast.error('Quantidade solicitada fora de estoque');
+  }
+
+  yield put(updateAmountSuccess(id, amount))
+}
+
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ])
